4266be6ae2fa89fdac50606223df89d7ada4cd0b
Luiz Paulo <lprodrigues451@gmail.com>
2022-05-23 18:30:44 -0300

    Subindo o arquivo que pratiquei em uma atividade para desenvolver um app que  mostra numero aleatorio de 1 ao 100

---
+++ app/src/main/java/com/example/toro/sorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.toro.sorteio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView textoResultado = findViewById(R.id.textoResultado);
+
+        int x = new Random().nextInt(101);
+
+        textoResultado.setText("O número sorteado foi : " + x);
+
+    }
+}
